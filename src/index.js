e51dded349476122109a305b9fef3cc211fd260e
ECD <ecofre@gmail.com>
2019-09-28 23:19:37 -0300

    Se configura proyecto inicial Node con Express y Nodemon

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const expressRestApp = require('./app');
+
+const port = process.env.PORT || 4444;
+
+expressRestApp.listen(port, () => {
+    console.log("Movies Node Rest API, running on port " + port);
+});
+
